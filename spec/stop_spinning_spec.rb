39c9507a464301e9739ecc16348e6ff26299ff0f
Dominik <dominik.alberski@gmail.com>
2020-02-10 20:07:31 +0100

    add specs for stop_spinning kata

---
+++ spec/stop_spinning_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require './stop_spinning'
+
+Rspec.describe 'spinWords' do
+  context 'for less then 5 letters' do
+    let(:sample) { 'stri' }
+    let(:sample_with_space) { "stri stri"}
+
+    it 'returns same string' do
+      expect(spinWords(sample)).to eq sample
+    end
+
+    it 'returns same string with spaces' do
+      expect(spinWords(sample_with_space)).to eq sample_with_space
+    end
+  end
+
+  context 'for 5 and more letter words it reverse that words' do
+    let(:sample) { 'string' }
+    let(:sample_2) { 'string string' }
+    let(:sample_3) { 'string stri string' }
+
+    it 'reverse string' do
+      expect(spinWords(sample)).to eq('gnirts')
+    end
+
+    it 'reverses strings with spaces' do
+      expect(spinWords(sample_2)).to eq('gnirts gnirts')
+    end
+
+    it 'reverse only words with 5 or more chard' do
+      expect(spinWords(sample_3)).to eq('gnirts stri gnirts')
+    end
+  end
+end
