5588a5b30eeb3d6810297958d2438c4d7c660c24
Dominik <dominik.alberski@gmail.com>
2020-06-23 11:45:46 +0200

    New kata description

---
+++ groceris_store.rb
@@ -0,0 +1,16 @@
+# Ever since you started work at the grocer, you have been faithfully logging
+# down each item and its category that passes through. One day, your boss walks
+# in and asks, "Why are we just randomly placing the items everywhere? It's too
+# difficult to find anything in this place!" Now's your chance to improve the
+# system, impress your boss, and get that raise!
+
+# The input is a comma-separated list with category as the prefix in the form
+# "fruit_banana". Your task is to group each item into the 4 categories Fruit,
+# Meat, Other, Vegetable and output a string with a category on each line
+# followed by a sorted comma-separated list of items.
+
+# For example, given:
+# "fruit_banana,vegetable_carrot,fruit_apple,canned_sardines,drink_juice,fruit_orange"
+
+# output:
+# "fruit:apple,banana,orange\nmeat:\nother:juice,sardines\nvegetable:carrot"
